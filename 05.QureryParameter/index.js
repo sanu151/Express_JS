5678dfbb360d41ba754cbd3a534adc79f045dae7
Supriyo Das <das.supriyo07@gmail.com>
2025-01-05 05:32:23 +0530

    HTTP request || Query Parameter

--- 05.QureryParameter/index.js
+++ 05.QureryParameter/index.js
@@ -3,8 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  const id = req.query.id;
-  const name = req.query.name;
+  const { id, name } = req.query;
   res.send(`<h1>Hi I am ${name}, My id is ${id}<h1>`);
   res.end();
 });
