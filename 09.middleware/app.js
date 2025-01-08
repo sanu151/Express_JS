3290828c1ec450ec26109353cf2f8b1c21250703
Supriyo Das <das.supriyo07@gmail.com>
2025-01-09 05:13:11 +0530

    Middleware in Express JS

--- 09.middleware/app.js
+++ 09.middleware/app.js
@@ -10,4 +10,13 @@ app.get("/", myMiddleware, (req, res) => {
   res.send(`<h1>Time now : ${req.currnetTime} </h1>`);
 });
 
+// Error Handle Middleware
+app.use((req, res, next) => {
+  res.send("<h1> 404 Bad URL request");
+});
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 module.exports = app;
